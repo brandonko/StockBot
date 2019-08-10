b2d63761e6ad266dbc3f2c85d8ecbb582c5beb63
sjhangiani12 <sjhangiani12@users.noreply.github.com>
2019-08-10 00:04:12 -0700

    fixed ' formatting error

--- parse_sa.py
+++ parse_sa.py
@@ -10,15 +10,13 @@ soup = BeautifulSoup(r.content, "lxml")
 
 data = {}
 next_attr = soup.find(attrs={"class" : "next"})
-r = requests.get(page)
-soup = BeautifulSoup(r.content, "lxml")
 temp_breaker_count = 0
 for ticker in soup.find_all(attrs={"class" : "mc"}):
     temp_breaker_count += 1
     ticker_name = ticker.find(attrs={"class" : "media-left"}).text.strip()
     news_title = ticker.find(attrs={"class" : "title"}).text.strip()
     bullets_header = ticker.find(attrs={"class" : "bullets"})
-    bullets = [bullet.text.strip() for bullet in bullets_header.find_all("li")]
+    bullets = [bullet.text.strip().replace("\'", "`") for bullet in bullets_header.find_all("li")]
     internal_json = {"title" : news_title, "bullets" : bullets}
     internal_json = json.dumps(internal_json)
     data[ticker_name] = internal_json
