ceb88cff8efb85b9101f6d7f6a1c89b419172205
sjhangiani12 <sjhangiani12@users.noreply.github.com>
2019-08-09 23:52:11 -0700

    added parser

---
+++ parse_sa.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import sys
+
+# get the link of the mainpage and convert it to beautiful soup for parsing
+page = "https://seekingalpha.com/market-news/all"
+r = requests.get(page)
+soup = BeautifulSoup(r.content, "lxml")
+
+data = {}
+next_attr = soup.find(attrs={"class" : "next"})
+r = requests.get(page)
+soup = BeautifulSoup(r.content, "lxml")
+temp_breaker_count = 0
+for ticker in soup.find_all(attrs={"class" : "mc"}):
+    temp_breaker_count += 1
+    ticker_name = ticker.find(attrs={"class" : "media-left"}).text.strip()
+    news_title = ticker.find(attrs={"class" : "title"}).text.strip()
+    bullets_header = ticker.find(attrs={"class" : "bullets"})
+    bullets = [bullet.text.strip() for bullet in bullets_header.find_all("li")]
+    internal_json = {"title" : news_title, "bullets" : bullets}
+    internal_json = json.dumps(internal_json)
+    data[ticker_name] = internal_json
+    if temp_breaker_count > 5:
+        break
+
+json_final_data = json.dumps(data)
+print(json_final_data)
+sys.stdout.flush()
